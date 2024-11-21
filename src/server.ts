c9f9727de785aeccd0e6d6b441929f7148e1137c
herbertdesousa <sousaherbert138@gmail.com>
2024-11-21 08:58:28 -0300

    feat: Add balance endpoint to server with formatted closing date

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { addDays, format } from 'date-fns';
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
+});
+
+app.get('/balance', (req, res) => {
+  res.json({
+    amount: '55.98',
+    closesIn: format(addDays(new Date(), 5), 'yyyy-MM-dd'),
+  });
 });
 
 app.listen(3333, () => {
